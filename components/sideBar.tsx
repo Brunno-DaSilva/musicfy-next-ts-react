61aef937db257adce53abdeccabc82d5010bb4cb
Bruno DaSilva <hi.dasilvab@gmail.com>
2022-06-03 12:05:29 -0500

    added initial code to side, imported dependencies and added logo

--- components/sideBar.tsx
+++ components/sideBar.tsx
@@ -1,7 +1,38 @@
-import { List } from "@chakra-ui/layout";
+import NextImage from "next/image";
+import {
+  List,
+  Box,
+  ListIcon,
+  ListItem,
+  Center,
+  Divider,
+  LinkOverlay,
+} from "@chakra-ui/layout";
+
+import {
+  MdHome,
+  MdSearch,
+  MdLibraryMusic,
+  MdPlaylistAdd,
+  MdFavorite,
+} from "react-icons/md";
 
 const Sidebar = () => {
-  return <div>SIDEBAR component 😎</div>;
+  return (
+    <Box
+      width="100%"
+      height="calc(100vh - 100px)"
+      bg="#3d00e0"
+      paddingX="5px"
+      color="gray"
+    >
+      <Box paddingY="20px">
+        <Box width="120px" marginBottom="20px" paddingY="20px">
+          <NextImage src="/b-logo-png-white.png" width={50} height={100} />
+        </Box>
+      </Box>
+    </Box>
+  );
 };
 
 export default Sidebar;
