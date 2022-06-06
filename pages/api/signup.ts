e607c81dc08b9429565a345a3ab388080742a80a
Bruno DaSilva <hi.dasilvab@gmail.com>
2022-06-06 11:40:31 -0500

    Added imports for the signup.ts

--- pages/api/signup.ts
+++ pages/api/signup.ts
@@ -1,5 +1,15 @@
 import bcrypt from "bcrypt";
 import jwt from "jsonwebtoken";
 import cookie from "cookie";
+import { NextApiRequest, NextApiResponse } from "next";
 
-export default async () => {};
+/*
+Strategy: 
+[ ] User sends credentials
+[ ] Attempt to create a new user with credentials and a rash password
+[ ] Either create the user or not generate feedback to user weather it fails or not. 
+[ ] Place the jsonwebtoken into the cookie to be store in the browser
+jsonwebtoken stores - user credentials - name, email, role, etc.
+*/
+
+export default async (req: NextApiRequest, res: NextApiResponse) => {};
