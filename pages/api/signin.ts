50deeb091b8e080be51a5077b51c8d585908cfe9
Bruno DaSilva <hi.dasilvab@gmail.com>
2022-06-06 12:56:15 -0500

    Added signin working

---
+++ pages/api/signin.ts
@@ -0,0 +1,52 @@
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
+import cookie from "cookie";
+import { NextApiRequest, NextApiResponse } from "next";
+import prisma from "../../lib/prisma";
+
+/*
+Strategy: 
+[ ] Expect an Email and Password 
+[ ] Check to see if the email already exist against the users in db
+[ ] Validate the user
+[ ] 
+*/
+
+export default async (req: NextApiRequest, res: NextApiResponse) => {
+  const { email, password } = req.body;
+
+  const user = await prisma.user.findUnique({
+    where: {
+      email,
+    },
+  });
+
+  //check to see if user's password match
+  if (user && bcrypt.compareSync(password, user.password)) {
+    const token = jwt.sign(
+      {
+        id: user.id,
+        email: user.email,
+        time: Date.now(),
+      },
+      "hello",
+      {
+        expiresIn: "8h",
+      }
+    );
+    res.setHeader(
+      "Set-Cookie",
+      cookie.serialize("MUSICFY_ACCESS_TOKEN", token, {
+        httpOnly: true,
+        maxAge: 8 * 60 * 60,
+        path: "/",
+        sameSite: "lax",
+        secure: process.env.NODE_ENV === "production",
+      })
+    );
+    res.json(user);
+  } else {
+    res.status(401);
+    res.json({ error: "Invalid Email or Password" });
+  }
+};
