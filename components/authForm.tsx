a63564dc9454de304ca2f4841a7f076104a0a731
Bruno DaSilva <hi.dasilvab@gmail.com>
2022-06-07 15:50:30 -0500

    Added authForm component

--- components/authForm.tsx
+++ components/authForm.tsx
@@ -1,3 +1,24 @@
 import { Box, Flex, Input, Button } from "@chakra-ui/react";
 import { useRouter } from "next/router";
 import { useSWRConfig } from "swr";
+import { useState } from "react";
+
+const AuthForm = ({ mode }) => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [isLoading, setIsLoading] = useState(false);
+  const router = useRouter;
+
+  return (
+    <Box height="100vh" width="100vw" bg="black" color="white">
+      <Flex justify="center" align="center" height="100px">
+        Hello
+      </Flex>
+      <Flex justify="center" align="center" height="calc(100vh-100px)">
+        Form
+      </Flex>
+    </Box>
+  );
+};
+
+export default AuthForm;
